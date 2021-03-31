369311d6babe617584dd9246f15062b56714a5e5
Rami Hanin <rami.hanin@gmail.com>
2021-03-31 17:39:03 -0500

    Parse command line arguments and help option

--- oss.cpp
+++ oss.cpp
@@ -1,3 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 int main(int argc, char* argv[]){
-    
+
+    //Command Line Parsing
+    string argNext = "";
+    for(int i  = 1; i < argc; i++){
+        string arg = argv[i];
+        if(arg == "-h"){
+            cout << "Help:" << endl;
+            cout << "master [-h] [-s t] [-l f]" <<endl;
+            cout << " -h : Help Options" <<endl;
+            cout << " -s : Indicate how many maximum seconds before the system terminates" <<endl;
+            cout << " -l : Specify a particular name for the log file" <<endl;
+            cout << "Example run: ./oss -s 3 -l log.txt" <<endl;
+            return 0;
+        } else if(arg == "-s"){
+            argNext = argv[i+1];
+            cout << "-s " << argNext << endl;
+        } else if(arg == "-l"){
+            argNext = argv[i+1];
+            cout << "-l " << argNext << endl;
+        }
+    }
 }
